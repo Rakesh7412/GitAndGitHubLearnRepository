a36cb599cbd3acf2be9aafc2dd6f27ceabdb10dc
Rakesh7421 <rkshelar73@gmail.com>
2026-03-22 19:24:37 +0530

    First commit

---
+++ src/com/day1/normal/FirstFile.java
@@ -0,0 +1,20 @@
+package com.day1.normal;
+
+public class FirstFile {
+	
+
+	public int add(int a, int b)
+	{
+		
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		FirstFile f1 = new FirstFile();
+		int i = f1.add(10, 20);
+		System.out.println("Addition is:"+i);
+		
+	}
+}
